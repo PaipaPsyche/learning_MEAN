90ec4ecae9451cd7d22011dc827128560bedc6a5
PaipaPsyche <paipamurderer@gmail.com>
2020-04-16 01:26:55 -0300

    Angular initial commit + models

---
+++ curso_mean2/client/src/app/models/album.ts
@@ -0,0 +1,11 @@
+export class Album{
+  constructor(
+    //public _id: string,
+    public title: string,
+    public description: string,
+    public year: number,
+    public password: string,
+    public artist: string,
+    public image: string
+  ){}
+}
---
+++ curso_mean2/client/src/app/models/song.ts
@@ -0,0 +1,10 @@
+export class Song{
+  constructor(
+    //public _id: string,
+    public name: string,
+    public number: number,
+    public duration: string,
+    public album: string,
+    public file: string
+  ){}
+}
